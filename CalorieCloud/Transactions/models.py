c790a8816713d546ba919b4ee1cdad683a821766
Derek Leung <xdmds@hotmail.com>
2013-10-19 01:33:57 -0400

    created CalorieCloud/Transactions/models.py

--- CalorieCloud/Transactions/models.py
+++ CalorieCloud/Transactions/models.py
@@ -2,3 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Transaction(models.Model):
+	"""
+	Description: Keeps track of all information from a Transaction
+	"""
+	name = models.CharField(max_length=254)
+	email = models.CharField(max_length=254)
+	time = models.DateTimeField()
+	amount = models.FloatField()
+	recipient = models.ForeignKey(User)	
+
+	def __str__(self):
+		return str(self) 
